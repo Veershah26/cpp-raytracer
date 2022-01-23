75b82b4237b177e4ee171f4a07e46583bdaf2875
Veershah26 <shah.veer18@gmail.com>
2022-01-24 00:10:19 +0530

    Added width and height

--- main.cpp
+++ main.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "Raytracer First Step.." << endl;
+
+    int width = 640;
+    int height = 480;
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            
+        }
+    }
+
     return 0;
 }
